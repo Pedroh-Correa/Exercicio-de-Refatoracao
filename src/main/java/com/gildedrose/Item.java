959f71e38001ff23a26cff62c69e5e4c14c49dc5
Pedroh-Correa <phrnunescorrea@gmail.com>
2025-10-23 20:46:09 -0300

    refatorado com as regras de negocio

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,20 +2,81 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private String name;
 
-    public int sellIn;
+    private int sellIn;
 
-    public int quality;
+    private int quality;
 
-    public Item(String name, int sellIn, int quality) {
+    private boolean conjured;
+
+    public Item(String name, int sellIn, int quality,  boolean conjured) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.conjured = conjured;
     }
 
-   @Override
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void updateQuality() {
+        if(this.sellIn == 0 && this.quality >=2) {
+            this.quality = this.quality - 2;
+        }else if(this.name.equals("Aged Brie")) {
+            this.quality++;
+        }else if (this.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }else if(this.name.equals("Backstage passes")) {
+            if(this.sellIn <= 10 && this.sellIn > 5) {
+                this.quality++;
+                this.quality ++;
+            }else if(this.sellIn <= 5 && this.sellIn > 0) {
+                this.quality++;
+                this.quality ++;
+                this.quality ++;
+            }else if(this.sellIn == 0) {
+                this.quality = 0;
+            }
+        }
+        else{// caso padrao onde so decresce o tempo de validade e a qualidade
+            this.quality --;
+            this.sellIn --;
+        }
+
+        this.sellIn --;
+
+        if(this.quality < 0) {
+            this.quality = 0;
+        }
+        if(this.quality >50) {
+            this.quality = 50;
+        }
+    }
+
+    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
+
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void agedBrie_QualityIncreases_AsItGetsOlder() {
+        Item agedBrie = new Item("Aged Brie", 10, 20, false);
+        Item meat = new Item("Meat", 8, 15, false);
+        Item[] items = new Item[] { agedBrie };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        for(Item item : items) {
+            item.updateQuality();
+        }
+
+
+        assertEquals(9, app.items[0].getSellIn());
+        assertEquals(21, app.items[0].getQuality());
+    }
+
+    @Test
+    void itemQuality_NeverExceeds_50() {
+        Item agedBrie = new Item("Aged Brie", 10, 50, false);
+        Item[] items = new Item[] { agedBrie };
+        GildedRose app = new GildedRose(items);
+
+        for(Item item : items) {
+            item.updateQuality();
+        }
+
+        assertEquals(50, app.items[0].getQuality());
+    }
+
+
+    @Test
+    void sulfuras_BeingLegendary_NeverChanges() {
+        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80, false);
+        Item[] items = new Item[] { sulfuras };
+        GildedRose app = new GildedRose(items);
+
+        for(Item item : items) {
+            item.updateQuality();
+        }
+
+        assertEquals(10, app.items[0].getSellIn());
+        assertEquals(80, app.items[0].getQuality());
     }
 
 }
